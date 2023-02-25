52c6d664c640551f46593d382de2dd256c4290c2
subrat <subratj611@gmail.com>
2023-02-25 16:45:21 +0530

    adding routes

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,20 +1,24 @@
+import { Outlet } from "react-router-dom";
 import { ReactComponent as CartIcon } from "../../assets/cart.svg";
 
 import "./navbar.scss";
 
 export default function NavBar() {
   return (
-    <div className="navbar-wrapper">
-      <div className="container">
-        <span className="logo">Game Store</span>
-        <ul className="nav-lists">
-          <li className="nav-list">
-            <CartIcon className="nav-cart-icon" />
-          </li>
-          <li className="nav-list">sign in</li>
-          <li className="nav-list btn-signup">sign up</li>
-        </ul>
+    <div>
+      <div className="navbar-wrapper">
+        <div className="container">
+          <span className="logo">Game Store</span>
+          <ul className="nav-lists">
+            <li className="nav-list">
+              <CartIcon className="nav-cart-icon" />
+            </li>
+            <li className="nav-list">sign in</li>
+            <li className="nav-list btn-signup">sign up</li>
+          </ul>
+        </div>
       </div>
+      <Outlet />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import SearchBar from "../components/SearchBar";
+
+export default function Home() {
+  return (
+    <div className="home-page-wrapper">
+      <SearchBar />
+    </div>
+  );
+}
