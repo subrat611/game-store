eaea3424aee10cbf0868c3c8aeca26097286da60
subrat <subratj611@gmail.com>
2023-02-26 15:33:01 +0530

    mobile nav menu add

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ReactComponent as CartIcon } from "../../assets/cart.svg";
 import { ReactComponent as MenuIcon } from "../../assets/menu-icon.svg";
@@ -5,6 +6,12 @@ import { ReactComponent as MenuIcon } from "../../assets/menu-icon.svg";
 import "./navbar.scss";
 
 export default function NavBar() {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  function toggleMenu() {
+    setToggleNav((prev) => !prev);
+  }
+
   return (
     <div>
       <div className="navbar-wrapper">
@@ -18,7 +25,14 @@ export default function NavBar() {
             <li className="nav-list btn-signup">sign up</li>
           </ul>
           <div className="hambar-menu">
-            <MenuIcon className="mob-nav-menu-icon" />
+            <MenuIcon className="mob-nav-menu-icon" onClick={toggleMenu} />
+            <ul className={toggleNav ? "mob-nav-lists show" : "mob-nav-lists"}>
+              <li className="mob-nav-list">
+                <CartIcon className="nav-cart-icon" />
+              </li>
+              <li className="mob-nav-list">sign in</li>
+              <li className="mob-nav-list btn-signup">sign up</li>
+            </ul>
           </div>
         </div>
       </div>
