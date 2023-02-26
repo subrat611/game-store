49c87d6834720f9ca5b092b61ca436bf489ab081
subrat <subratj611@gmail.com>
2023-02-26 12:25:34 +0530

    filtered search functionality implement

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,12 +1,17 @@
 import "./searchbar.scss";
 
-export default function SearchBar() {
+export default function SearchBar({ searchForGames }) {
+  function handleSearchText(searchText) {
+    searchForGames(searchText);
+  }
+
   return (
     <div className="search-bar-wrapper">
       <input
         placeholder="search here..."
         type="text"
         className="search-input"
+        onChange={(e) => handleSearchText(e.target.value)}
       />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,28 +4,52 @@ import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   const [games, setGames] = useState([]);
+  const [searchGames, setSearchGames] = useState([]);
 
   useEffect(() => {
-    setTimeout(() => {
-      loadGames();
-    }, 1000);
+    if (games.length <= 0) loadGames();
+    searchForGames();
   }, []);
 
   async function loadGames() {
-    fetch("/data.json")
-      .then((res) => res.json())
-      .then((data) => setGames(data));
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "73bde6a55amsh023835711d23438p138d37jsnfec285384beb",
+        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
+
+    if (games.length <= 0) {
+      fetch(
+        "https://free-to-play-games-database.p.rapidapi.com/api/games",
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => setGames(response))
+        .catch((err) => console.error(err));
+    }
+  }
+
+  function searchForGames(searchText) {
+    if (searchText !== "") {
+      const searchListGames = games.filter(
+        (game) => game.title.toLowerCase().indexOf(searchText) !== -1
+      );
+      setSearchGames(searchListGames);
+    } else setSearchGames([]);
   }
 
   return (
     <div className="home-page-wrapper">
-      <SearchBar />
+      <SearchBar searchForGames={searchForGames} />
       <ul className="game-lists-wrapper">
-        {games.length > 0 ? (
-          games.map((game) => <GameList key={game.id} game={game} />)
-        ) : (
-          <li className="game-list-loader">Loading...</li>
-        )}
+        {games.length > 0 &&
+          searchGames.length <= 0 &&
+          games.map((game) => <GameList game={game} key={game.id} />)}
+
+        {searchGames.length > 0 &&
+          searchGames.map((game) => <GameList game={game} key={game.id} />)}
       </ul>
     </div>
   );
