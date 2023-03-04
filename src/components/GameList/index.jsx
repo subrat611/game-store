286ab84c918011c348b3206717ad9ef83ff9ddc7
subrat <subratj611@gmail.com>
2023-03-04 23:27:41 +0530

    link update

--- src/components/GameList/index.jsx
+++ src/components/GameList/index.jsx
@@ -1,22 +1,25 @@
+import { Link } from "react-router-dom";
 import "./gamelist.scss";
 
 export default function GameList({ game }) {
   return (
-    <li className="game-list">
-      <div className="game-card">
-        <div className="game-card-img">
-          <img src={game.thumbnail} alt="game-image" />
-        </div>
-        <div className="game-card-body">
-          <div className="body-genre">
-            <span>{game.genre}</span>
-            <span>{game.platform}</span>
+    <Link to={`/game-details/${game.id}`}>
+      <li className="game-list">
+        <div className="game-card">
+          <div className="game-card-img">
+            <img src={game.thumbnail} alt="game-image" />
           </div>
-          <div className="body-title">
-            <p>{game.title}</p>
+          <div className="game-card-body">
+            <div className="body-genre">
+              <span>{game.genre}</span>
+              <span>{game.platform}</span>
+            </div>
+            <div className="body-title">
+              <p>{game.title}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   );
 }
